b8494efdc542e4086af565156aebbda4298db263
Noryu <HanaNoryu@outlook.com>
2023-03-23 09:54:27 +0800

    chore: add function about package.

---
+++ api/index.ts
@@ -0,0 +1,6 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node'
+import pkg from '../package.json'
+
+export default (req: VercelRequest, res: VercelResponse) => {
+  res.json(pkg)
+}
